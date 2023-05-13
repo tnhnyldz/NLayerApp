0245a0978a4057796b59e915fccf4ce2c85c0188
Tunahan Yildiz <tunahanyildiz@outlook.it>
2023-05-13 22:21:47 +0300

    db context and configuration files

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.CoreLayer;
+using NLayer.Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //scan assembly and find configration classes whice inherits IEntityTypeConfiguration<entity>
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //other way
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+    }
+}
