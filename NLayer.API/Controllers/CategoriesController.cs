ae9faa6a970756b8ab04ee6c5010e53ab1cb0063
Tunahan Yildiz <tunahanyildiz@outlook.it>
2023-08-05 18:20:12 +0300

    service changed

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -21,27 +21,27 @@ namespace NLayer.API.Controllers
         [HttpGet("[action]/{categoryId}")]
         public async Task<IActionResult> GetSingleCategoryByIdWithProductsAsync(int categoryId)
         {
-            // Mevcut HTTP isteği için HttpContext nesnesini al
-            HttpContext httpContext = _contextAccessor.HttpContext;
+            //// Mevcut HTTP isteği için HttpContext nesnesini al
+            //HttpContext httpContext = _contextAccessor.HttpContext;
 
-            // İstek bilgilerini kullanarak bazı özelliklere eriş
-            string clientIpAddress = httpContext.Connection.RemoteIpAddress.ToString();
-            string userAgent = httpContext.Request.Headers["User-Agent"];
-            string requestPath = httpContext.Request.Path;
-            string requestMethod = httpContext.Request.Method;
-            var requestHeaders = httpContext.Request.Headers;
+            //// İstek bilgilerini kullanarak bazı özelliklere eriş
+            //string clientIpAddress = httpContext.Connection.RemoteIpAddress.ToString();
+            //string userAgent = httpContext.Request.Headers["User-Agent"];
+            //string requestPath = httpContext.Request.Path;
+            //string requestMethod = httpContext.Request.Method;
+            //var requestHeaders = httpContext.Request.Headers;
 
-            HttpRequest request = _contextAccessor.HttpContext.Request;
+            //HttpRequest request = _contextAccessor.HttpContext.Request;
 
 
-            string scheme = request.Scheme;         // http veya https
-            string host = request.Host.Host;        // örn. localhost
-            int? port = request.Host.Port;           // örn. 7072
-            string pathBase = request.PathBase;     // örn. /api
-            string path = request.Path;             // örn. /categories
-            string queryString = request.QueryString.ToString();  // örn. ?param1=value1&param2=value2
+            //string scheme = request.Scheme;         // http veya https
+            //string host = request.Host.Host;        // örn. localhost
+            //int? port = request.Host.Port;           // örn. 7072
+            //string pathBase = request.PathBase;     // örn. /api
+            //string path = request.Path;             // örn. /categories
+            //string queryString = request.QueryString.ToString();  // örn. ?param1=value1&param2=value2
 
-            string fullUrl = $"{scheme}://{host}:{port}{pathBase}{path}{queryString}";
+            //string fullUrl = $"{scheme}://{host}:{port}{pathBase}{path}{queryString}";
 
 
 
